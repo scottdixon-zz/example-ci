28c5dcbb3458ed8fa3d0c16b513f935f5d5d544b
Scott Dixon <scott@howrefreshing.com.au>
2017-11-30 11:03:58 +1000

    Created GET /api/dogs endpoint

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('Server!!!');
+const express = require('express')
+const app = express()
+
+const dogs = ['Doggo', 'Pupper', 'Woofer']
+
+app.get('/api/dogs', (req, res) => {
+  // Send the dogs back to the user
+  res.send(dogs)
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
