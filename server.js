1c9021b16c01e3482f9827fec35b100cdc6e0112
Scott Dixon <scott@howrefreshing.com.au>
2017-12-12 08:44:05 +1000

    dogs & owners

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express()
 
 const dogs = ['Doggo', 'Pupper', 'Woofer']
+const owners = ['John', 'Sam', '']
 
-app.get('/api/dogs', (req, res) => {
-  // Send the dogs back to the user
-  res.send(dogs)
-})
+app.get('/api/dogs', (req, res) => res.send(dogs))
+
+app.get('/api/owners', (req, res) => res.send(owners))
+
 
 app.listen(3001, () => console.log('Example app listening on port 3000!'))
